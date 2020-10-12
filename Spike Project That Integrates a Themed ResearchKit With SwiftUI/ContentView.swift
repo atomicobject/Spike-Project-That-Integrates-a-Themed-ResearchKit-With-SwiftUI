0321000f0850062dacf2be8d634c5d9d797706db
Joe Chrysler <mail@joechrysler.com>
2020-10-12 15:12:51 -0400

    Initial Commit

---
+++ Spike Project That Integrates a Themed ResearchKit With SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spike Project That Integrates a Themed ResearchKit With SwiftUI
+//
+//  Created by Joe Chrysler on 10/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spike Project That Integrates a Themed ResearchKit With SwiftUI/Spike_Project_That_Integrates_a_Themed_ResearchKit_With_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spike_Project_That_Integrates_a_Themed_ResearchKit_With_SwiftUIApp.swift
+//  Spike Project That Integrates a Themed ResearchKit With SwiftUI
+//
+//  Created by Joe Chrysler on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Spike_Project_That_Integrates_a_Themed_ResearchKit_With_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
